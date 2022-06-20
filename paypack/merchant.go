2cbd63f24d9857908faddc269ec8e98136173e62
iradukunda1 <iradujdd4@gmail.com>
2022-06-20 11:26:41 +0200

    add merchant id on merchant profile

--- paypack/merchant.go
+++ paypack/merchant.go
@@ -3,6 +3,7 @@ package paypack
 import "context"
 
 type Merchant struct {
+	ID      string  `json:"id",omitempty`
 	Name    string  `json:"name,omitempty"`
 	InRate  float64 `json:"in_rate,omitempty"`
 	OutRate float64 `json:"out_rate,omitempty"`
